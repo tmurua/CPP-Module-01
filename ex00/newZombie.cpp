b52706d4b98494efa78088ddf00d6c11225b343a
tmurua <tommurua@gmail.com>
2025-02-27 16:16:01 +0100

    feat: use new operator to create object objecy dynamically allocating its memory on the heap, so object can be used outside the scope of the function newZombie

--- ex00/newZombie.cpp
+++ ex00/newZombie.cpp
@@ -1,21 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*   newZombie.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tmurua <tmurua@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 13:57:51 by tmurua            #+#    #+#             */
-/*   Updated: 2025/02/27 14:10:22 by tmurua           ###   ########.fr       */
+/*   Updated: 2025/02/27 16:14:25 by tmurua           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-// creates a zombie, names it, returns the zombie created.
+// new operator creates an object that stays around even after function ends
 Zombie* newZombie(std::string name)
 {
-	Zombie nZombie(name);
+	Zombie* nZombie = new Zombie(name);
 
-	return &nZombie;
+	return nZombie;
 }
